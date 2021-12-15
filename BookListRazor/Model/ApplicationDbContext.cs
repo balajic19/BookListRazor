8f54d7e7ab3c3ae4c48f0fdda8d4ea9846a1d271
Balaji Chandupatla <balajichandupatla1@gmail.com>
2021-12-14 19:44:45 -0500

    Add project files.

---
+++ BookListRazor/Model/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    // Whenever we are dealing with database, we need ApplicationDbContext
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // In order to add any model to the database, we need an entry here
+        public DbSet<Book> Book { get; set; }
+    }
+}
